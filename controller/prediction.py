e486efd37793cb0ea96a4bdb6b3ae6ee6c8b581d
Juhan <dungthcsnt2014@gmail.com>
2025-07-03 15:57:53 +0700

    Commit

--- controller/prediction.py
+++ controller/prediction.py
@@ -18,8 +18,8 @@ async def predict_churn(
     background_tasks: BackgroundTasks,
     file: UploadFile = File(...),
     model_version: str = Form(default="1"),
-    scaler_version: str = Form(default="scaler_churn_version_20250701T105905.pkl"),
-    run_id: str = Form(default="b523ba441ea0465085716dcebb916294"),
+    scaler_version: str = Form(default="scaler_churn_version_20250703T150200.pkl"),
+    run_id: str = Form(default="d70ecaf6c6e84537826bdbad64166436"),
     model_name: str = Form(default="RandomForestClassifier"),
 ):
     return await ChurnController.predict_churn(background_tasks=background_tasks, file=file, model_version=model_version, scaler_version=scaler_version, run_id=run_id, model_name=model_name)
